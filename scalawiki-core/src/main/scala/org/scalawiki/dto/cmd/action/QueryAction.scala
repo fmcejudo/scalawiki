f4f48ce57509a641f03d62d09281ccca6a5d7d31
francisco <fmcejudo@gmail.com>
2016-11-12 14:22:53 +0000

    Create Query and Parse Action as possible implementation of the request to media wiki

--- scalawiki-core/src/main/scala/org/scalawiki/dto/cmd/action/QueryAction.scala
+++ scalawiki-core/src/main/scala/org/scalawiki/dto/cmd/action/QueryAction.scala
@@ -1,5 +1,6 @@
 package org.scalawiki.dto.cmd.action
 
+import org.scalawiki.dto.cmd.parse.Parse
 import org.scalawiki.dto.cmd.{EnumArg, EnumParameter}
 import org.scalawiki.dto.cmd.query.Query
 
@@ -15,10 +16,10 @@ case class Action(override val arg: ActionArg) extends EnumParameter[ActionArg](
   override def toString = pairs.toString()
 }
 
-case class PageAction(){
-
+case class QueryActions(override val arg: ActionArg) extends Action(arg){
+  override def query: Option[Query] = args.collect { case q: Query => q }.headOption
 }
 
-case class ParseAction(){
-
+case class ParseAction(override val arg: ActionArg) extends Action(arg){
+  override def query: Option[Parse] = args.collect { case p: Parse => p }.headOption
 }
